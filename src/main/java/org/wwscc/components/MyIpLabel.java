0996169637d51b283bf027aa1cf6e6d7b0747320
Brett Wilson <drytoastman@gmail.com>
2017-08-11 01:29:49 -0700

    hopefully better job at getting the external ip when machine is multi-homed (as all docker-machine installs will be)

--- src/main/java/org/wwscc/components/MyIpLabel.java
+++ src/main/java/org/wwscc/components/MyIpLabel.java
@@ -7,7 +7,9 @@ package org.wwscc.components;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
@@ -31,9 +33,22 @@ public class MyIpLabel extends JLabel implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		try {
+			DatagramSocket s = new DatagramSocket();
+			s.connect(InetAddress.getByAddress(new byte[]{1,1,1,1}), 0);
+			setText("My IP: " + s.getLocalAddress().getHostAddress());
+			s.close();
+			return;
+		} catch (SocketException | UnknownHostException se) {
+			Logger.getLogger(MyIpLabel.class.getName()).info("My IP attempt 1 failed: " + se);
+		} 
+		
+		try {
 			setText("My IP: " + InetAddress.getLocalHost().getHostAddress());
+			return;
 		} catch (UnknownHostException ex) {
-			Logger.getLogger(MyIpLabel.class.getName()).info("Failed to get local IP: " + ex);
+			Logger.getLogger(MyIpLabel.class.getName()).info("My IP attempt 2 failed: " + ex);
 		}
+		
+		setText("My IP: Unknown");
 	}
 }
