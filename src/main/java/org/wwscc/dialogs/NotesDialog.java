bbb3b7d0cf206cca9329dc2bc21fc8116c293e72
Brett Wilson <drytoastman@gmail.com>
2018-04-01 14:49:09 -0700

    notes dialog border

--- src/main/java/org/wwscc/dialogs/NotesDialog.java
+++ src/main/java/org/wwscc/dialogs/NotesDialog.java
@@ -9,6 +9,8 @@
 package org.wwscc.dialogs;
 
 import javax.swing.JTextPane;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -21,6 +23,15 @@ public class NotesDialog extends BaseDialog<String>
         super(new MigLayout("fill, gap 2"), true);
         pane = new JTextPane();
         pane.setText(notes);
+
+        UIDefaults system = UIManager.getDefaults();
+        UIDefaults changes = new UIDefaults();
+        changes.put("TextPane[Enabled].borderPainter", system.get("TextField[Enabled].borderPainter"));
+        changes.put("TextPane[Focused].borderPainter", system.get("TextField[Focused].borderPainter"));
+        changes.put("TextPane[Enabled].backgroundPainter", system.get("TextField[Enabled].backgroundPainter"));
+        pane.putClientProperty("Nimbus.Overrides", changes);
+        pane.putClientProperty("Nimbus.Overrides.InheritDefaults", true);
+
         mainPanel.add(pane, "h 300, w 300, grow");
     }
 
