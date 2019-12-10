8fcfdc42dcd5da14eb47a0fc04d2a90e63aa2eb3
Brett Wilson <drytoastman@gmail.com>
2019-12-10 13:23:18 -0800

    update for new build

--- createdist.py
+++ createdist.py
@@ -7,19 +7,16 @@ import subprocess
 import sys
 
 
-BASH = """
-#!/usr/bin/env sh
-cd $(dirname ${{BASH_SOURCE[0]}})
-bin/javaw -classpath {classpath} {mainclass}
+BASH = """#!/usr/bin/env sh
+cd $(dirname ${{BASH_SOURCE[0]}})/bin
+java -classpath {classpath} {mainclass}
 """
 
-BATCH = """
-cd %~dp0bin
+BATCH = """cd %~dp0bin
 start javaw.exe -classpath {classpath} {mainclass}
 """
 
-WINRULES = """
-%echo off
+WINRULES = """%echo off
 
 CALL :addrule ScorekeeperWeb       TCP 80
 CALL :addrule ScorekeeperTimers    TCP 54328
@@ -49,7 +46,9 @@ EXIT /B 0
 class Dist():
 
     def __init__(self, args):
+        print(args)
         self.__dict__.update(vars(args))
+        self.builddir = "build/runtime/"+self.target
         self.pairs = [
             ('StartScorekeeper', 'org.wwscc.system.ScorekeeperSystem'),
             ('LoadCerts',        'org.wwscc.system.LoadCerts'),
@@ -58,30 +57,35 @@ class Dist():
 
 
     def basejre(self):
-        subprocess.run(["jlink", "-v", "--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages", 
-                         "--module-path", self.jdk+"/jmods/", "--output", self.dest, "--add-modules", self.modules], check=True)
+        if not os.path.exists(self.builddir):
+            subprocess.run(["jlink", "-v", "--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages", 
+                             "--module-path", self.jdk+"/jmods/", "--output", self.builddir, "--add-modules", self.modules], check=True)
 
     def copyapp(self):
-        #self.basejre()
-        libdir = os.path.join(self.app, "lib")
+        self.basejre()
         self.jars = list()
+
+        libdir = os.path.join(self.app, "lib")
         for f in os.listdir(libdir):
             self.jars.append(f)
-            shutil.copy(os.path.join(libdir,f), os.path.join(self.dest, "lib"))
+            shutil.copy(os.path.join(libdir,f), os.path.join(self.builddir, "lib"))
         
 
     def createdist(self):
         self.copyapp()
-        if 'win' in self.jdk:
+        if self.target == 'win':
             self.createScripts(BATCH, ".bat", ";")
-            self.executableFile(os.path.join(self.dest, "rules.bat"), WINRULES)
+            self.executableFile(os.path.join(self.builddir, "rules.bat"), WINRULES)
         else:
             self.createScripts()
+
+        name = "build/runtime/Scorekeeper-{version}-{target}".format(**self.__dict__)
+        shutil.make_archive(name, "zip", self.builddir)
 
     def createScripts(self, template=BASH, ext="", separator=":"):
         classpath = separator.join(map(lambda x: os.path.join("../lib",x), self.jars))
         for name, mainclass in self.pairs:
-            self.executableFile(os.path.join(self.dest, name+ext), template.format(classpath=classpath, mainclass=mainclass))
+            self.executableFile(os.path.join(self.builddir, name+ext), template.format(classpath=classpath, mainclass=mainclass))
 
     def executableFile(self, path, data):
         with open(path, "w") as fp:
@@ -92,9 +96,10 @@ class Dist():
 
 parser = argparse.ArgumentParser(description='Create distribution')
 parser.add_argument('--modules', required=True)
-parser.add_argument('--jdk', required=True)
-parser.add_argument('--app', required=True)
-parser.add_argument('--dest', required=True)
+parser.add_argument('--jdk',     required=True)
+parser.add_argument('--app',     required=True)
+parser.add_argument('--target',  required=True)
+parser.add_argument('--version', required=True)
 args = parser.parse_args()
 
 Dist(args).createdist()
