c6f0dac6dd6b97f3bd410057c3683d7c121e4e01
Brett Wilson <drytoastman@gmail.com>
2020-01-16 04:03:01 -0800

    I guess that just worked out in earlier java versions but not with openjdk12, different values

--- src/main/java/org/wwscc/dataentry/TimeEntry.java
+++ src/main/java/org/wwscc/dataentry/TimeEntry.java
@@ -674,7 +674,7 @@ public class TimeEntry extends JPanel implements ListSelectionListener, ListData
         @Override
         public void keyTyped(KeyEvent e)
         {
-            int increment = ((e.getModifiersEx() & ActionEvent.SHIFT_MASK) == ActionEvent.SHIFT_MASK) ? -1 : 1;
+            int increment = ((e.getModifiersEx() & KeyEvent.SHIFT_DOWN_MASK) == KeyEvent.SHIFT_DOWN_MASK) ? -1 : 1;
             switch(e.getKeyChar())
             {
                 case 'c':
